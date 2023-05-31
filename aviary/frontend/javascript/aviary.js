8a883b138e089e331ef8cc887f6488cd4a4a508d
Sijie Wang <3463757+sijieamoy@users.noreply.github.com>
2023-05-31 13:34:09 -0700

    Add noscript for google (#96)
    Add `<noscript>` for Google Search Console validation.
    
    ---------
    
    Signed-off-by: Antoni Baum <antoni.baum@protonmail.com>
    Co-authored-by: Antoni Baum <antoni.baum@protonmail.com>

--- aviary/frontend/javascript/aviary.js
+++ aviary/frontend/javascript/aviary.js
@@ -5,6 +5,18 @@
   gtm.text =
     "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5ZPDX2P');";
   head.insertBefore(gtm, head.children[0]);
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const body = document.getElementsByTagName("body")[0];
+    var noscript = document.createElement("noscript");
+    var iframe = document.createElement("iframe");
+    iframe.src = "https://www.googletagmanager.com/ns.html?id=GTM-5ZPDX2P";
+    iframe.height = "0";
+    iframe.width = "0";
+    iframe.style = "0";
+    noscript.appendChild(iframe);
+    body.insertBefore(noscript, body.children[0]);
+});
 })();
 
 // Set favicon
