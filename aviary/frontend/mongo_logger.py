a085ccb39e95ca5214b82fd64688ceaf6da2a585
Max Pumperla <max.pumperla@googlemail.com>
2023-06-14 08:51:29 +0200

    minor MongoLogger improvements (#124)
    Signed-off-by: Max Pumperla <max.pumperla@googlemail.com>

--- aviary/frontend/mongo_logger.py
+++ aviary/frontend/mongo_logger.py
@@ -1,11 +1,13 @@
 import uuid
 from datetime import datetime, timezone
-from typing import Any
+from typing import Any, TYPE_CHECKING
+if TYPE_CHECKING:
+    from gradio.components import IOComponent
 
 from gradio import FlaggingCallback
 from pymongo import MongoClient
 
-from aviary.common.constants import COLLECTION_NAME, DB_NAME
+from aviary.common.constants import COLLECTION_NAME, DB_NAME, NUM_LLM_OPTIONS
 from aviary.common.llm_event import LlmEvent, LlmResponse, Vote
 
 
@@ -17,13 +19,15 @@ class MongoLogger(FlaggingCallback):
         self.client = MongoClient(url)
         self.project_name = project_name
         self.components = None
+        self.db = None
         try:
             self.client.admin.command("ping")
             print("Pinged MongoDB. Correctly set up")
         except Exception as e:
             print(e)
 
-    def setup(self, components):
+    def setup(self, components: list[IOComponent], flagging_dir: str = None):
+        """FlaggingCallback-compliant setup method."""
         self.components = components
         # Check if the database exists
         if DB_NAME in self.client.list_database_names():
@@ -54,22 +58,17 @@ class MongoLogger(FlaggingCallback):
             created_at=datetime.now(timezone.utc),
             instance_id=str(uuid.uuid4()),
             user_prompt=flag_data[0],
-            # TODO(mwk): Work out how to generalize this to _n_ inputs
             responses=[
                 LlmResponse(
-                    model_id=flag_data[1], text=flag_data[4], gen_stats=flag_data[8][0]
-                ),
-                LlmResponse(
-                    model_id=flag_data[2], text=flag_data[5], gen_stats=flag_data[8][1]
-                ),
-                LlmResponse(
-                    model_id=flag_data[3], text=flag_data[6], gen_stats=flag_data[8][2]
-                ),
+                    model_id=flag_data[i],  # 1, 2, 3
+                    text=flag_data[i + NUM_LLM_OPTIONS],  # 4, 5, 6
+                    gen_stats=flag_data[-2][i-1]  # 0, 1, 2
+                ) for i in range(1, NUM_LLM_OPTIONS + 1)
             ],
-            session_id=flag_data[9],
+            session_id=flag_data[-1],
         )
-        if flag_data[7]:
-            vote_number = int(flag_data[7][-1])
+        if flag_data[-3]:
+            vote_number = int(flag_data[-3][-1])
             event.votes = Vote(llm=flag_data[vote_number], score=1)
 
         print(f"Event is {event.json()}")
