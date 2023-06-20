69a2b99af3809da730f7f2afc2b1c4dcf112c799
Max Pumperla <max.pumperla@googlemail.com>
2023-06-20 11:04:52 +0200

    gradio variable types (#135)
    @waleedkadous not sure if useful, but at least this is a structured way
    to de/compose variables passed into Gradio that drives me less crazy.
    Not particularly opinionated about this, but the "flat list passing +
    slicing" is maddening.
    
    Signed-off-by: Max Pumperla <max.pumperla@googlemail.com>

---
+++ aviary/frontend/types.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Selection:
+
+    def __init__(self, button, choices: List):
+        self.button = button
+        self.choices = choices
+
+    def to_list(self):
+        return [self.button] + self.choices
+
+    @classmethod
+    def from_list(cls, lst):
+        return lst[0], list(lst[1:])
