5b7ec0e821a600b8a85ce9487b99c91765c03f7e
akshay-anyscale <122416226+akshay-anyscale@users.noreply.github.com>
2023-08-03 22:08:58 -0700

    Update request.py
    Signed-off-by: akshay-anyscale <122416226+akshay-anyscale@users.noreply.github.com>

--- template/request.py
+++ template/request.py
@@ -1,12 +1,28 @@
 import requests
-
-resp = requests.post(
-    "http://127.0.0.1:8000/lightgpt/stream/", json={"prompt": "What is the best place to eat in San Francisco?","use_prompt_format":"true"}, stream = True
+from aviary.backend.server.models import (
+    Args,
+    AviaryModelResponse,
+    Prompt,
 )
 
-import json
+
+prompt = Prompt(prompt="What is the best place to eat in San Francisco?",
+            parameters={
+                "max_new_tokens": 128,
+            },)
+
+resp = requests.post(
+    "http://127.0.0.1:8000/lightgpt/stream/", json={
+                    "prompt": prompt.dict(),
+                    "priority":0,
+                }, stream = True
+)
 resp.raise_for_status()
+import json
 for chunk in resp.iter_content(chunk_size=None, decode_unicode=True):
-    print(json.loads(chunk)["generated_text"], end="", flush=True)
+    #print(chunk["generated_text"])
+    token = json.loads(chunk)["generated_text"]
+    if (token != None):
+        print(token, end="", flush=True)
 
 print("")
