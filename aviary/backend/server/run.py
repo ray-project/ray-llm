a54ef3386ebc2573d1427b16deff744800522976
Antoni Baum <antoni.baum@protonmail.com>
2023-10-03 16:34:06 -0700

    Revert "Fix serve config parsing (#66)" (#68)
    This reverts commit b9bdaac5e4dbbd1a51ab94855fc91aeb0cf4eeed.

--- aviary/backend/server/run.py
+++ aviary/backend/server/run.py
@@ -7,7 +7,7 @@ from ray import serve
 from aviary.backend.llm.vllm.vllm_engine import VLLMEngine
 from aviary.backend.llm.vllm.vllm_models import VLLMApp
 from aviary.backend.server.app import RouterDeployment
-from aviary.backend.server.models import LLMApp, RouterArgs, ScalingConfig
+from aviary.backend.server.models import LLMApp, ScalingConfig
 from aviary.backend.server.plugins.deployment_base_client import DeploymentBaseClient
 from aviary.backend.server.plugins.execution_hooks import (
     ExecutionHooks,
@@ -111,8 +111,7 @@ def router_deployment(
 
 
 def router_application(args):
-    router_args = RouterArgs.parse_obj(args)
-    llm_apps = parse_args(router_args.models, llm_app_cls=VLLMApp)
+    llm_apps = parse_args(args, llm_app_cls=VLLMApp)
     return router_deployment(llm_apps, enable_duplicate_models=False)
 
 
